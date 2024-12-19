020cd3a4d44a20baa7278052be3af7ac2d2b3201
Sky <IzalithSky@users.noreply.github.com>
2024-12-19 18:18:22 +0000

    visuals added

--- game_objects/mobs/mob.gd
+++ game_objects/mobs/mob.gd
@@ -2,15 +2,22 @@ extends CharacterBody2D
 
 @onready var ray_cast_2d = $RayCast2D
 @export var move_speed = 100
+@export var attack_range = 60
 
 @onready var player : CharacterBody2D = get_tree().get_first_node_in_group("player")
+@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 
 var alive = true
+
+
+func _ready() -> void:
+	anim.play("run")
+
 
 func _physics_process(delta):
 	if not alive:
 		return
-	
+		
 	if player == null:
 		return
 		
@@ -18,16 +25,20 @@ func _physics_process(delta):
 	velocity = dir_to_player * move_speed
 	move_and_slide()
 	
-	global_rotation = dir_to_player.angle() + PI / 2.0
+	if velocity.x < 0:
+		anim.flip_h = true
+	else:
+		anim.flip_h = false
 	
-	if ray_cast_2d.is_colliding() and ray_cast_2d.get_collider() == player:
+	#global_rotation = dir_to_player.angle() + PI / 2.0
+	
+	if global_position.distance_to(player.global_position) <= attack_range:
+		anim.play("attack")
 		player.kill()
 	
 func kill():
 	if not alive:
 		return
 	alive = false
-	$graphics/dead.show()
-	$graphics/alive.hide()
 	$CollisionShape2D.disabled = true
-	z_index = -1
+	anim.play("death")
--- game_objects/player/player.gd
+++ game_objects/player/player.gd
@@ -2,19 +2,34 @@ extends CharacterBody2D
 
 
 @onready var ray_cast_2d = $RayCast2D
-@export var move_speed = 200
+@export var move_speed = 150
+
+@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 
 var alive = true
+
 
 func _process(delta):
 	if not alive:
 		return
-
-	global_rotation = global_position.direction_to(get_global_mouse_position()).angle() + PI / 2.0
+		
+	ray_cast_2d.global_rotation = ray_cast_2d.global_position.direction_to(
+		get_global_mouse_position()).angle() + PI / 2.0
 	
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 		
+	if velocity != Vector2.ZERO:
+		anim.play("run")
+	else:
+		anim.play("idle")
+		
+	if velocity.x < 0:
+		anim.flip_h = true
+	else:
+		anim.flip_h = false
+
+
 func _physics_process(delta):
 	if not alive:
 		return
@@ -22,18 +37,15 @@ func _physics_process(delta):
 	var move_dir = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	velocity = move_dir * move_speed
 	move_and_slide()
-	
+
+
 func kill():
 	if not alive:
 		return
 	alive = false
-	$graphics/dead.show()
-	$graphics/alive.hide()
-	z_index = -1
-		
-func shoot():
-	$graphics/flash.show()
-	$graphics/flash/Timer.start()
-	
+	anim.play("death")
+
+
+func shoot():	
 	if ray_cast_2d.is_colliding() and ray_cast_2d.get_collider().has_method("kill"):
 		ray_cast_2d.get_collider().kill()
